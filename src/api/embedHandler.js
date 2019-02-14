fe5cc4f8b553eadafef1ac899a1dd3ecb0c053b9
NullDev (Chris) <shadow@nulldev.org>
2019-02-14 02:20:20 +0100

    initial embed handler

---
+++ src/api/embedHandler.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Dependencies
+let request = require("request");
+let moment = require("moment");
+
+// Utils
+let log = require("../utils/logger");
+
+moment.locale("de");
+
+// TODO
